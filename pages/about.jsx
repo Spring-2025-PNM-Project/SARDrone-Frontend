034643ceae72d9204d4137a09b9b2e8bffcea892
kevintsoii <kevin.tsoi44@gmail.com>
2025-03-19 21:34:54 -0700

    reformat

---
+++ pages/about.jsx
@@ -0,0 +1,13 @@
+// pages/index.js
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="container">
+      <h1 className="text-red-500">hello from about page!</h1>
+      <h1>u have 5 seconds before ur pc blows up!!!</h1>
+    </div>
+  );
+};
+
+export default Home;
---
+++ pages/index.jsx
@@ -0,0 +1,10 @@
+// pages/index.js\
+import React from "react";
+
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-blue-500">
+      <h1 className="text-blue text-4xl">Tailwind CSS is Working!</h1>
+    </div>
+  );
+}
